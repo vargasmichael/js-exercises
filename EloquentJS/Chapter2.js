c063b521fcb4ea74511e2b039e824eb4c02476a9
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-08-14 10:18:46 -0600

    fizz buzz answer and refactor

--- EloquentJS/Chapter2.js
+++ EloquentJS/Chapter2.js
@@ -44,12 +44,36 @@
 //######
 //#######
 
-for (let string = "#"; string <= "#######"; string = string += "#") {
-  console.log(string);
-}
-//refactor
+// for (let string = "#"; string <= "#######"; string = string += "#") {
+//   console.log(string);
+// }
+// //refactor
 
-for (let string = "#"; string.length < 8; string += "#") {
-  console.log(string);
-}
+// for (let string = "#"; string.length < 8; string += "#") {
+//   console.log(string);
+// }
 //using string.length is more efficient than fully writing out the desired length
+
+//Fizz Buzz
+
+for (let i = 0; i <= 100; i++) {
+  if (i === 7) {
+    console.log(i);
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+//refactored
+
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
